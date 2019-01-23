a267a26d0d93df97cd077947006fe8cd0bc4db9b
Jiayi Liu <liujiayi19920724@gmail.com>
2019-01-23 19:30:37 +1300

    request location sharing permission during and outside the app

--- GeoTriggerTest/ViewController.swift
+++ GeoTriggerTest/ViewController.swift
@@ -7,14 +7,75 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        
     }
-
-
+    
+    @IBAction func callToAction(_ sender: Any) {
+        // check authorization status
+        enableLocationServices()
+    }
+    
+    //MARK:- location manager
+    func enableLocationServices() {
+        locationManager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            // Request when-in-use authorization initially
+            locationManager.requestWhenInUseAuthorization()
+            break
+            
+        case .restricted, .denied:
+            // Disable location features
+            disableMyLocationBasedFeatures()
+            break
+            
+        case .authorizedWhenInUse:
+            // Enable basic location features
+            escalateLocationServiceAuthorization()
+            break
+            
+        case .authorizedAlways:
+            // Enable any of your app's location features
+            enableMyAlwaysFeatures()
+            break
+        }
+    }
+    
+    func escalateLocationServiceAuthorization() {
+        // Escalate only when the authorization is set to when-in-use
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("please enable always share location!")
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+    
+    func disableMyLocationBasedFeatures(){
+        
+    }
+    
+    func enableMyWhenInUseFeatures(){
+        
+    }
+    
+    func enableMyAlwaysFeatures(){
+        // coordinate of SLI
+        // Latitude = -43.535822, Longitude = 172.639964
+        
+        //
+        print("location is always shared!")
+    }
 }
 
