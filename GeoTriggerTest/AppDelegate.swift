fd7a95e31afc542fcc9bf8dcb22fe6e80981cd6a
Jiayi Liu <liujiayi19920724@gmail.com>
2019-01-26 20:06:34 +1300

    fire notification when user enters the WestField region

--- GeoTriggerTest/AppDelegate.swift
+++ GeoTriggerTest/AppDelegate.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import CoreLocation
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var locationManager: CLLocationManager?
+    var notificationCenter : UNUserNotificationCenter = UNUserNotificationCenter.current()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
@@ -28,13 +30,50 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        
+        let sliSystemsCoordinate = CLLocationCoordinate2DMake(-43.535822, 172.639964)
+        let sliSystemsRegion = CLCircularRegion(center: sliSystemsCoordinate,
+                                                radius: 300,
+                                                identifier: "sliSystemsRegion")
+//        handleEvent(forRegion: sliSystemsRegion)
     }
 
 }
 extension AppDelegate : CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+    
+    func locationManager(_ manager : CLLocationManager, didEnterRegion region : CLRegion) {
+        
+        print("enters region : \(region.identifier)")
+        
         // send the notification for region
+        handleEvent(forRegion: region)
+    }
+    
+    func handleEvent(forRegion region:CLRegion){
+        // name the region
+        let regionIdentifier = region.identifier.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        
+        // create notification
+        let content = UNMutableNotificationContent()
+        content.title = "You have arrived"
+        content.body = "Region : \(regionIdentifier)"
+        content.sound = UNNotificationSound.default
+        
+        // after user enters, how long to trigger the notification
+        let timeInSeconds : TimeInterval = 10
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInSeconds,
+                                                        repeats: false)
+        
+        // send notification request
+        let request = UNNotificationRequest(identifier: "user_enters_\(regionIdentifier)",
+                                            content: content,
+                                            trigger: trigger)
+        
+        // trying to add the notification request to notification center
+        notificationCenter.add(request, withCompletionHandler: { (error) in
+            if error != nil {
+                print("Error adding notification with identifier: \(regionIdentifier)")
+            }
+        })
     }
 }
 
--- GeoTriggerTest/ViewController.swift
+++ GeoTriggerTest/ViewController.swift
@@ -77,19 +77,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UNUserNotific
         print("location is always shared!")
         // Your coordinates go here (lat, lon)
         let sliSystemsCoordinate = CLLocationCoordinate2DMake(-43.535822, 172.639964)
+        let sliSystemsCoordinate2 = CLLocationCoordinate2DMake(-43.530901, 172.59834)
         
         /* Create a region centered on desired location,
          choose a radius for the region (in meters)
          choose a unique identifier for that region */
         let sliSystemsRegion = CLCircularRegion(center: sliSystemsCoordinate,
-                                              radius: 100,
+                                              radius: 300,
                                               identifier: "sliSystemsRegion")
-        
-        
         sliSystemsRegion.notifyOnEntry = true
         sliSystemsRegion.notifyOnExit = false
         
-        locationManager.startMonitoring(for: sliSystemsRegion)
+        // westfield : Latitude = -43.530901, Longitude = 172.59834
+        let westfieldRegion = CLCircularRegion(center: sliSystemsCoordinate2,
+                                         radius: 400,
+                                         identifier: "WestField")
+        westfieldRegion.notifyOnEntry = true
+        westfieldRegion.notifyOnExit = false
+        
+
+        locationManager.startMonitoring(for: westfieldRegion)
         
         // ask for notification permission
         getNotificationPermission()
